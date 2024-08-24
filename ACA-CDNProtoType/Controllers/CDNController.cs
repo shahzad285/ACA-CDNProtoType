e94154f183dff4bb32e1dc8f6e3ce34cda573b52
DESKTOP-S59BJJM\personal <shahzadahamad285@gmail.com>
2024-08-24 18:17:08 +0530

    CDN controller and memory cache code added

---
+++ ACA-CDNProtoType/Controllers/CDNController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.Net.Http;
+
+namespace ACA_CDNProtoType.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CDNController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CDNController(IMemoryCache cache, IHttpClientFactory httpClientFactory)
+        {
+            _cache = cache;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("{key}")]
+        public async Task<IActionResult> Get(string key)
+        {
+            if (_cache.TryGetValue(key, out string cachedValue))
+            {
+                // Return the cached value
+                return Ok(cachedValue);
+            }
+            else
+            {
+                // Forward the request to Google if not found in cache
+                var client = _httpClientFactory.CreateClient();
+                var response = await client.GetAsync($"https://www.google.com/search?q={key}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    // Cache the response content
+                    _cache.Set(key, content, TimeSpan.FromMinutes(10));
+
+                    // Return the response content
+                    return Ok(content);
+                }
+
+                return StatusCode((int)response.StatusCode);
+            }
+        }
+
+    }
+}
--- ACA-CDNProtoType/Program.cs
+++ ACA-CDNProtoType/Program.cs
@@ -1,12 +1,14 @@
+using Microsoft.Extensions.Caching.Memory;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddMemoryCache();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
